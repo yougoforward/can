6e4a3e88d4bfd8a743b5c25b0efd882de97dfa76
yougoforward <908865817@qq.com>
2020-05-02 12:34:04 +0800

    path

--- experiments/segmentation/scripts/deeplabv3plus_res50_pcontext.sh
+++ experiments/segmentation/scripts/deeplabv3plus_res50_pcontext.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #train
-python train.py --dataset pcontext \
-    --model deeplabv3plus --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 --batch-size 8 \
-    --backbone resnet50 --checkname deeplabv3plus_res50_pcontext
-
+python experiments.segmentation.train.py --dataset pcontext \
+    --model deeplabv3plus --aux --dilated --multi-grid --stride 16 --aackbone resnet50 --checkname deeplabv3plus_res50_pcontext
+trous-rates 6 12 18 --base-size 520 --crop-size 520 --batch-size 8 \
+    --b
 #test [single-scale]
-python test.py --dataset pcontext \
+python experiments.segmentation.test.py --dataset pcontext \
     --model deeplabv3plus --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume runs/pcontext/deeplabv3plus/deeplabv3plus_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
-python test.py --dataset pcontext \
+python experiments.segmentation.test.py --dataset pcontext \
     --model deeplabv3plus --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume runs/pcontext/deeplabv3plus/deeplabv3plus_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
