e614ed266064d52758eeff0767f15133ab33eb4b
yougoforward <908865817@qq.com>
2020-05-02 12:07:00 +0800

    dilation

--- experiments/segmentation/scripts/can_res50_pcontext.sh
+++ experiments/segmentation/scripts/can_res50_pcontext.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #train
 python train_can.py --dataset pcontext \
-    --model can --aux --dilated --multi-grid --stride 16 --base-size 520 --crop-size 520 \
+    --model can --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --checkname can_res50_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
-    --model can --aux --dilated --multi-grid --stride 16 --base-size 520 --crop-size 520 \
+    --model can --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume runs/pcontext/can/can_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
-    --model can --aux --dilated --multi-grid --stride 16 --base-size 520 --crop-size 520 \
+    --model can --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume runs/pcontext/can/can_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
--- experiments/segmentation/scripts/deeplabv3plus_res50_pcontext.sh
+++ experiments/segmentation/scripts/deeplabv3plus_res50_pcontext.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #train
 python train.py --dataset pcontext \
-    --model deeplabv3plus --aux --dilated --multi-grid --stride 16 --base-size 520 --crop-size 520 \
+    --model deeplabv3plus --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --checkname deeplabv3plus_res50_pcontext
 
 #test [single-scale]
 python test.py --dataset pcontext \
-    --model deeplabv3plus --aux --dilated --base-size 520 --crop-size 520 \
+    --model deeplabv3plus --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume runs/pcontext/deeplabv3plus/deeplabv3plus_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python test.py --dataset pcontext \
-    --model deeplabv3plus --aux --dilated --base-size 520 --crop-size 520 \
+    --model deeplabv3plus --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume runs/pcontext/deeplabv3plus/deeplabv3plus_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
