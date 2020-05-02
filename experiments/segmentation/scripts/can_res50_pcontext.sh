64ebd4e830711cc71cd06f624a2a15c0b4ebb3d8
yougoforward <908865817@qq.com>
2020-05-02 12:36:36 +0800

    path

--- experiments/segmentation/scripts/can_res50_pcontext.sh
+++ experiments/segmentation/scripts/can_res50_pcontext.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #train
-python experiments.segmentation.train_can.py --dataset pcontext \
+python experiments/segmentation/train_can.py --dataset pcontext \
     --model can --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 --batch-size 8 \
     --backbone resnet50 --checkname can_res50_pcontext
 
 #test [single-scale]
-python experiments.segmentation.test.py --dataset pcontext \
+python experiments/segmentation/test.py --dataset pcontext \
     --model can --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume runs/pcontext/can/can_res50_pcontext/model_best.pth.tar --split val --mode testval
 
 #test [multi-scale]
-python experiments.segmentation.test.py --dataset pcontext \
+python experiments/segmentation/test.py --dataset pcontext \
     --model can --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume runs/pcontext/can/can_res50_pcontext/model_best.pth.tar --split val --mode testval --ms
