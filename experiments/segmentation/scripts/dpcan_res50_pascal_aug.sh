565685a1c9f5b81c6f65920bc2192e5a3a8b867c
yougoforward <908865817@qq.com>
2020-06-16 09:41:30 +0800

    first commit

--- experiments/segmentation/scripts/dpcan_res50_pascal_aug.sh
+++ experiments/segmentation/scripts/dpcan_res50_pascal_aug.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
 # train
-python -m experiments.segmentation.train_can --dataset psacal_aug \
+python -m experiments.segmentation.train_can --dataset pascal_aug \
     --model dpcan --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --checkname dpcan_res50_psacal_aug
+    --backbone resnet50 --checkname dpcan_res50_pascal_aug
 
 #test [single-scale]
-python -m experiments.segmentation.test --dataset psacal_aug \
+python -m experiments.segmentation.test --dataset pascal_aug \
     --model dpcan --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume experiments/segmentation/runs/psacal_aug/dpcan/dpcan_res50_psacal_aug/checkpoint.pth.tar --split val --mode testval
+    --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/dpcan/dpcan_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval
 
 #test [multi-scale]
-python -m experiments.segmentation.test --dataset psacal_aug \
+python -m experiments.segmentation.test --dataset pascal_aug \
     --model dpcan --aux --dilated --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume experiments/segmentation/runs/psacal_aug/dpcan/dpcan_res50_psacal_aug/checkpoint.pth.tar --split val --mode testval --ms
+    --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/dpcan/dpcan_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval --ms
