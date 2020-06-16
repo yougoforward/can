39ec3fcee2e1424c0ae35dc8ea5eb1e56158c71c
yougoforward <908865817@qq.com>
2020-06-16 21:26:33 +0800

    option

--- experiments/segmentation/scripts/deeplabv3_res50_pascal_aug.sh
+++ experiments/segmentation/scripts/deeplabv3_res50_pascal_aug.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
 # train
-python -m experiments.segmentation.train --dataset psacal_aug \
+python -m experiments.segmentation.train --dataset pascal_aug \
     --model deeplabv3 --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
-    --backbone resnet50 --checkname deeplabv3_res50_psacal_aug
+    --backbone resnet50 --checkname deeplabv3_res50_pascal_aug
 
 #test [single-scale]
-python -m experiments.segmentation.test --dataset psacal_aug \
+python -m experiments.segmentation.test --dataset pascal_aug \
     --model deeplabv3 --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume experiments/segmentation/runs/psacal_aug/deeplabv3/deeplabv3_res50_psacal_aug/checkpoint.pth.tar --split val --mode testval
+    --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/deeplabv3/deeplabv3_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval
 
 #test [multi-scale]
-python -m experiments.segmentation.test --dataset psacal_aug \
+python -m experiments.segmentation.test --dataset pascal_aug \
     --model deeplabv3 --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume experiments/segmentation/runs/psacal_aug/deeplabv3/deeplabv3_res50_psacal_aug/checkpoint.pth.tar --split val --mode testval --ms
+    --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/deeplabv3/deeplabv3_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval --ms
