6c9af67bc8dcde5d1484cad651e0394575176504
yougoforward <908865817@qq.com>
2020-06-17 19:11:00 +0800

    dpcan

--- experiments/segmentation/scripts/dpcan_res50_pascal_aug.sh
+++ experiments/segmentation/scripts/dpcan_res50_pascal_aug.sh
@@ -1,8 +1,8 @@
 # !/usr/bin/env bash
 # train
-# python -m experiments.segmentation.train_can --dataset pascal_aug \
-#     --model dpcan --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
-#     --backbone resnet50 --checkname dpcan_res50_pascal_aug
+python -m experiments.segmentation.train_can --dataset pascal_aug \
+    --model dpcan --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
+    --backbone resnet50 --checkname dpcan_res50_pascal_aug
 
 #test [single-scale]
 python -m experiments.segmentation.test --dataset pascal_aug \
