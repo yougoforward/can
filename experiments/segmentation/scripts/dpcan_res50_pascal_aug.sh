56a8296961ae77609bc30c8a710930a9c860716b
yougoforward <908865817@qq.com>
2020-06-22 11:18:33 +0800

    eval_whole

--- experiments/segmentation/scripts/dpcan_res50_pascal_aug.sh
+++ experiments/segmentation/scripts/dpcan_res50_pascal_aug.sh
@@ -5,11 +5,11 @@ python -m experiments.segmentation.train_can --dataset pascal_aug \
     --backbone resnet50 --checkname dpcan_res50_pascal_aug
 
 #test [single-scale]
-python -m experiments.segmentation.test --dataset pascal_aug \
+python -m experiments.segmentation.test_whole --dataset pascal_aug \
     --model dpcan --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/dpcan/dpcan_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval
 
 #test [multi-scale]
-python -m experiments.segmentation.test --dataset pascal_aug \
+python -m experiments.segmentation.test_whole --dataset pascal_aug \
     --model dpcan --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
     --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/dpcan/dpcan_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval --ms
