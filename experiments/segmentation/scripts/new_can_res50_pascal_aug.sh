615d9d902793da7dea2dceed7ddbb2191ba55980
yougoforward <908865817@qq.com>
2020-06-26 20:17:34 +0800

    new_can

--- experiments/segmentation/scripts/new_can_res50_pascal_aug.sh
+++ experiments/segmentation/scripts/new_can_res50_pascal_aug.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
 # train
 python -m experiments.segmentation.train_can --dataset pascal_aug \
-    --model dpcan --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
-    --backbone resnet50 --checkname dpcan_res50_pascal_aug
+    --model new_can --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
+    --backbone resnet50 --checkname new_can_res50_pascal_aug
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset pascal_aug \
-    --model dpcan --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/dpcan/dpcan_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval
+    --model new_can --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
+    --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/new_can/new_can_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset pascal_aug \
-    --model dpcan --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
-    --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/dpcan/dpcan_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval --ms
+    --model new_can --aux --dilated --multi-grid --stride 16 --atrous-rates 6 12 18 --base-size 520 --crop-size 520 \
+    --backbone resnet50 --resume experiments/segmentation/runs/pascal_aug/new_can/new_can_res50_pascal_aug/checkpoint.pth.tar --split val --mode testval --ms
